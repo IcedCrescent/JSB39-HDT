0806f9776c0197ccf3b0df7c94ff55ed371bbe94
IcedCrescent <ntn201@gmail.com>
2022-08-28 21:08:07 +0700

    Lesson 4

---
+++ Lesson-5/app.js
@@ -0,0 +1,55 @@
+
+// alert("Hello!");
+// alert("It's me");
+// alert("Trung");
+
+/* 
+    Multiline
+    comment
+*/
+console.log("Hello from console!");
+console.log("My name is", "Trung", "I'm ", 24);
+
+// Variable + Basic Data types
+
+// Declare
+let username = "Trung ! 23423 😡"; // String
+let age = 24; // Number
+let height = 1.83; // Number
+let isTeacher = true; // Boolean (true, false)
+
+console.log(username, age, height, isTeacher);
+
+// Re-assign
+username = 'Mai';
+username = `Huy`; // Backtick
+
+console.log(username, age, height, isTeacher);
+
+// let welcomeMessage = "Hello, welcome to my website";
+// alert(welcomeMessage);
+// let name = prompt("What is your name?");
+// alert("Hello " + name);
+// console.log("Hello", name);
+
+// Basic arimethic
+// + - * /
+let a = 5 * (1 + 1);
+console.log(a);
+console.log("String 1 " + "String 2");
+console.log("1" + 2);
+// Prompt returns a string
+age = parseInt(prompt("How old are you?"));
+// parseInt("20") => 20
+// parseInt("abc") => NaN
+let birthYear = 2022 - age;
+alert("You were born in " + birthYear);
+
+// Enter a
+// Enter b
+// a + b
+// a - b
+// a * b
+// a / b
+
+
