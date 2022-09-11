54b123b7850660ad2f2dcf1e1f5803e2d6d8b92e
IcedCrescent <ntn201@gmail.com>
2022-09-11 20:06:16 +0700

    lesson 5 cont

---
+++ Lesson-5/catchup.js
@@ -0,0 +1,42 @@
+// variable
+
+
+// Declaration
+let name = 'Trung 23432 😗'; // String
+
+let age = 24; // Number
+
+let result = (10 + 2) * 3;
+
+let isOver18 = false; // Boolean
+
+// using
+console.log(name, age, result);
+
+// re assign
+name = "Minh";
+console.log(name)
+
+age = 2022 - 2007;
+
+console.log(age);
+
+// let a = prompt("a = "); // string
+// a = parseInt(a); // number
+// let b = prompt ("b = "); // string
+// b = parseInt(b); // number
+// console.log(a + b);
+
+let str = "this is  a string";
+
+console.log(str.length);
+console.log(str[str.length - 2]);
+
+console.log(str.substring(5))
+console.log(str.substring(5, 7))
+
+// String interpolation
+console.log(`My name is ${name}`)
+
+// String concat
+console.log("My name is " + name)
